34a55cd9efef4c3c867aa2fbe2a0b7d757804083
yeonhan <yeonhanlab@gmail.com>
2026-04-20 15:41:56 +0900

    Search 컴포넌트 작성완료

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,8 +3,10 @@ import { type ChangeEvent, type SubmitEvent, useState } from "react";
 import { useNavigate } from "react-router";
 
 
-function SearchBar() {
+function SearchBar() { // 사용자에게 입력을 ㅡ받아서, 그걸 /search라고 하는 주소로 강제 이동 시켜야함
     const navigate = useNavigate(); // navigate라고 하는 변수에 이동에 관련된 기능을 담아둬야 함
+                                    // 이동에 관련된 기능을 담고 있는 변수를 만들어서, 그 변수를 사용해서 이동을 시킴
+                                    // 링크는 요소에다 쓰는거고ㅓ 이건 함수를 써야하는거
     const [keyword, setKeyword] = useState(""); // input에 잉렵되는 값을 관리할 목적의 state // 빈스트링!!
 
         const onSubmit = (event: SubmitEvent<HTMLFormElement>) => {
@@ -12,14 +14,14 @@ function SearchBar() {
             // 이걸 하지 못하도록 해야 함.
             event.preventDefault();
 
-            // 사용자가 그냥 엔터 쳐버리는 경우
-            if (keyword === "") return;
-
             // 생각보다, 이 input에 스페이스바를 넣는 애들도 엄청 많음 "수학" =>" 수학"
             // string의 맨 앞과 맨 뒤에 존재할 수 있는 공백을 제거하는 매서드 : trim()
             // "" 에서 사용자가 enter -> keyword.trim() -> ""  -> if (keyword.trim()) -> false
             // " " 에서 사용자가 enter -> keyword.trim() -> ""
             if (!keyword.trim()) return; // trim을 했더니, 값이 빈 스트링이면 return으로 끝내라
+
+            // 사용자가 그냥 엔터 쳐버리는 경우
+            if (keyword === "") return;
 
             // Keyword를 활용해서 사용자를 강제 이동
             // search 라고 하는 경우에, query string으로 keyword를 전달해서 사용자 이동
--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -1,5 +1,8 @@
-import { useSearchParams } from "react-router";
+import { Link, useSearchParams } from "react-router";
 import { useEffect, useState } from "react";
+import styles from "./Search.module.css";
+
+const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
 type BookItem = {
     id: string;
@@ -7,31 +10,83 @@ type BookItem = {
         title: string;
         author?: string[];
         description?: string;
+        publishedDatd: string;
         imageLinks?: {
             thumbnail?: string;
-            small?: string;
-        }
-    }
-}
+            smallThumbnail?: string;
+        };
+    };
+};
 
+// 사용자가 요청한 keyword를 받아서, 그것을 가지고 google API 요청을 하고, 받아온 결과를 화면에 출력해주는 일
+// keyword를 쿼리스트링으로 받겠다
+
+type ApiResponseType = { items: BookItem[] };
 
 function Search() {
-    // query string으로 들어온 값을 꺼내오기 위해서는 useSearchParams를 사용
+    // 사용자가 요청한 keyword를 받아서, 그것을 가지고 google API 요청을 하고, 받아온 결과를 화면에 출력해주는 일
+    // keyword를 쿼리스트링으로 받겠다.
 
+    // query string으로 들어온 값을 꺼내오기 위해서는 useSearchParams를 사용
     // useParams를 사용할 때는 const { id } = useParams();
     // useSearchParams는 useState와 사용법이 동일
-    const [params, setParams] = useSearchParams();    // queryString 내용이 params에 담겨 나옴
-    const k = params.get("keyword");     // "수학"이라는게 있을 수도 있지만, 없을 수도 있음 (수정할 수도 있는 내용이니까)
+    const [searchParams] = useSearchParams(); // queryString 내용이 params에 담겨 나옴
+    // 이렇게 가져온 params라고 하는 state의 값은 객체
+    const keyword = searchParams.get("keyword"); // "수학"이라는게 있을 수도 있지만, 없을 수도 있음 (수정할 수도 있는 내용이니까)
 
     // keyword 준비 됐으니, API를 통해 요청한 정보를 받아다가 화면에 출력만 해주면 됨
-    const [loading] = useState(true);
-    const [list, setList] = useState([]);
+    const [list, setList] = useState<BookItem[]>([]); // []는 알겠어. 근데 그 안에 들어가는 요소의 타입이 뭐야?
+    // 모르겠으니까, 요소는 없다고 생각할게 => never[]
 
     useEffect(() => {
-        fetch(``)
-    }, []);
+        if (!keyword) {
+            return;
+        }
 
-    return <></>;
+        fetch(
+            `https://www.googleapis.com/books/v1/volumes?q=${keyword}&maxResults=20&key=${API_KEY}`,
+        ) // 앞으로 데이터를 받아서 이 array요소가 들어갈거니까 이걸로 해줘. 이걸 알려면 API를 띄워봐야함
+            .then(response => response.json())
+            .then((json: ApiResponseType) => {
+                // 데이터를 받아왔고, 그거에 대해서 자바스크립트 형태로 가공도 했으니
+                // 그걸 list라고 하는 state에 저장해야지
+                setList(json.items);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }, [keyword]);
+
+    return (
+        <div className={styles.wrap}>
+            <h3>검색 결과 : {keyword}</h3>
+
+            {/* 검색 결과 (책 목록) 출력 */}
+            {/* 데이터가 도착했는지 안했는지, 목록이 있는지 없는지 판단 해줘야 되나? */}
+            {list.map((value, index) => (
+                <Link key={index} to={`/detail/${value.id}`} className={styles.item}>
+                    {value.volumeInfo.imageLinks ? (
+                        <img
+                            src={value.volumeInfo.imageLinks?.thumbnail}
+                            alt={value.volumeInfo.title}
+                            className={styles.cover}
+                        />
+                    ) : (
+                        <div className={styles.noCover}>No Cover</div>
+                    )}
+                    <div>
+                        <div className={styles.title}>{value.volumeInfo.title}</div>
+                        {/*
+                            array에서 사용할 수 있는 메소드 join(스트링)
+                            각 요소를 순회해서 하나의 값을 리턴하는데
+                            각 요소 사이에 [매개변수로 제공된 스트링]을 넣어준다.
+                        */}
+                        <div className={styles.authours}>{value.volumeInfo.author?.join(", ")}</div>
+                    </div>
+                </Link>
+            ))}
+        </div>
+    );
 }
 
 export default Search;
